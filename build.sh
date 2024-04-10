cc73c2a0c1d7d8852a949c8c26e0df397afc9773
k99k5 <545551320@qq.com>
2024-04-11 00:34:02 +0800

    init

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ mv ./dist/client ./../www
 cd ./../www || exit
 sed -i 's/="\//="\.\//g' index.html
 cd ./..
-npx condova platform add android
+npx cordova platform add android
 npx cordova run android
 npx cordova-hcp build

4eefd696a44dfa4d5e634a0d55ec324c938dacc4
k99k5 <545551320@qq.com>
2024-04-11 21:00:12 +0800

    init

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 rm ./www -rf
 cd src || exit
+git pull
 pnpm install
 pnpm build:client
 mv ./dist/client ./../www
 cd ./../www || exit
-sed -i 's/="\//="\.\//g' index.html
+#sed -i 's/="\//="\.\//g' index.html
 cd ./..
+echo "{}" > ./platforms/android/android.json
 npx cordova platform add android
 npx cordova run android
 npx cordova-hcp build
